f35d97f7254d2aa2305d49ecd5cc9444b8e4dd94
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-16 13:21:11 +0100

    Delete remnants of old code

--- backend/index.js
+++ backend/index.js
@@ -29,15 +29,6 @@ app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then((note) => {
     response.json(note);
   });
-
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
 });
 
 app.delete("/api/notes/:id", (request, response) => {
