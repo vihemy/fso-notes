017d47648c26f3c43e2968b8dcf8418a7e480c2b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-16 11:50:25 +0100

    Uncomment code

--- backend/mongo.js
+++ backend/mongo.js
@@ -25,10 +25,10 @@ const note = new Note({
   important: true,
 });
 
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
