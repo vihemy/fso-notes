00daa53c6e7cbb3609a82782b428418698a9370b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-17 11:13:22 +0100

    Remove old unused code

--- backend/index.js
+++ backend/index.js
@@ -38,10 +38,6 @@ app.get("/api/notes/:id", (request, response, next) => {
 app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
