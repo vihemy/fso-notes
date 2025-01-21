e22a36422ef1838b1189b89e8753a6c760796c3c
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-21 13:19:24 +0100

    Add conditional logic to to variable declaration

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+console.log('NODE_ENV is:', process.env.NODE_ENV)
+
 
 module.exports = {
   MONGODB_URI,
