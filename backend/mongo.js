ca81f2ceb46615dca4eba9382cd96565b616330c
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-21 13:55:01 +0100

    Add script to manually input to database

---
+++ backend/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const url = process.env.TEST_MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
+
+  const Note = mongoose.model('Note', noteSchema)
+
+  const note = new Note({
+    content: 'CSS is hard',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
