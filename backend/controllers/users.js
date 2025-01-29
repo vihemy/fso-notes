617f68f5df0f644e6cdf4a990a854e0b1e16d21c
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-29 20:48:42 +0100

    Populate notes field with note data

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -20,7 +20,10 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (requests, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({})
+    .populate('notes', { content: 1, important: 1 })
+
   response.json(users)
 })
 
