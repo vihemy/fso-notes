cedbacbbdaf50359d021232e89a40f6bd0206f1d
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-11 21:22:39 +0100

    Merge configs from backend into frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,16 +3,18 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist/**', 'build/**'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
@@ -22,12 +24,23 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic/js': stylisticJs,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': [2, 'never'],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-console': 'off',
+      'no-unused-vars': 'warn',
       'react/prop-types': 'off', // TEMP FIX https://stackoverflow.com/questions/38684925/react-eslint-error-missing-in-props-validation
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
