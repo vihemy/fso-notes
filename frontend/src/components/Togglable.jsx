2b50882e2396d1226670b2c394a3ad1a55a4fd66
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-17 21:26:23 +0100

    Add ref to component

--- frontend/src/components/Togglable.jsx
+++ frontend/src/components/Togglable.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState, forwardRef, useImperativeHandle } from 'react'
 
-const Togglable = (props) => {
+const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
@@ -10,18 +10,24 @@ const Togglable = (props) => {
     setVisible(!visible)
   }
 
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
   return (
     <div>
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
       <div style={showWhenVisible}>
-
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
     </div>
   )
-}
+
+})
 
 export default Togglable
