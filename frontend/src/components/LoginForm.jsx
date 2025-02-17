f74ca0f905636016d21101e6681171ac9a9c5d20
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-17 20:43:59 +0100

    Copy components from FSO github repo
    To avoid transcription mistakes

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,25 +1,34 @@
-const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-  
-  export default LoginForm;
+const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+ }) => {
+ return (
+   <div>
+     <h2>Login</h2>
+
+     <form onSubmit={handleSubmit}>
+       <div>
+         username
+         <input
+           value={username}
+           onChange={handleUsernameChange}
+         />
+       </div>
+       <div>
+         password
+         <input
+           type="password"
+           value={password}
+           onChange={handlePasswordChange}
+         />
+     </div>
+       <button type="submit">login</button>
+     </form>
+   </div>
+ )
+}
+
+export default LoginForm
--- frontend/src/components/NoteForm.jsx
+++ frontend/src/components/NoteForm.jsx
@@ -1,11 +1,17 @@
-const NoteForm = ({ addNote, newNote, handleNoteChange }) => (
-    <form onSubmit={addNote}>
-      <input
-        value={newNote}
-        onChange={handleNoteChange}
-      />
-      <button type="submit">save</button>
-    </form>
-  );
-  
-  export default NoteForm;
+const NoteForm = ({ onSubmit, handleChange, value }) => {
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={handleChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default NoteForm
