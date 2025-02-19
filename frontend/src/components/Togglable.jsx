ca144c6184160e27dd4b7e660326f8a0a60f01aa
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-19 20:04:53 +0100

    Add PropTypes

--- frontend/src/components/Togglable.jsx
+++ frontend/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
@@ -27,7 +28,10 @@ const Togglable = forwardRef((props, refs) => {
       </div>
     </div>
   )
-
 })
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
