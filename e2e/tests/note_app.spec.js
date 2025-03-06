a35988fe61b182b9f0bf19cfad0d86d9f7049bae
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-06 12:36:15 +0100

    Test front page and loginform

---
+++ e2e/tests/note_app.spec.js
@@ -0,0 +1,23 @@
+const { test, describe, expect } = require('@playwright/test')
+
+
+describe('Note app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  })
+
+  test('login form can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByRole('textbox').first().fill('mluukkai')
+    await page.getByRole('textbox').last().fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+  
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
+})
