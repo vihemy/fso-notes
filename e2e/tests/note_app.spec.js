86ff6133159a1e19eab913d1d22e6f3c3e3dfc94
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-09 11:54:08 +0100

    Add tests for login and note creation

--- e2e/tests/note_app.spec.js
+++ e2e/tests/note_app.spec.js
@@ -21,4 +21,28 @@ describe('Note app', () => {
   
     await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
+  
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByTestId('username').fill('mluukkai')
+    await page.getByTestId('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'log in' }).click()
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+  })  
 })
