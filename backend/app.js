5673d3e3c4d41ddfeac543ca5af13700bf1f676f
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-09 12:18:33 +0100

    Duplicate code from FSO repo
    To avoid typos or other hidden bugs

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
-require('express-async-errors')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -19,7 +20,7 @@ mongoose.connect(config.MONGODB_URI)
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+    logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(cors())
--- e2e/tests/note_app.spec.js
+++ e2e/tests/note_app.spec.js
@@ -2,7 +2,16 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 
 describe('Note app', () => {
-    beforeEach(async({page}) => {
+    beforeEach(async({page, request}) => {
+      await request.post('http://localhost:3001/api/testing/reset')
+      await request.post('http://localhost:3001/api/users', {
+        data: {
+          name: 'Matti Luukkainen',
+          username: 'mluukkai',
+          password: 'salainen'
+        }
+      })
+
         await page.goto('http://localhost:5173')
     })
     
@@ -43,6 +52,19 @@ describe('Note app', () => {
       await page.getByRole('textbox').fill('a note created by playwright')
       await page.getByRole('button', { name: 'save' }).click()
       await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+  
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
     })
   })  
 })
